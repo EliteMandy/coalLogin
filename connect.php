679d2c663aa3eadc174404aab0b5583b3d8dfc0c
guyVicky <vikrantsingh1889@gmail.com>
2023-09-12 19:45:17 +0530

    added changes

--- connect.php
+++ connect.php
@@ -1,18 +1,10 @@
 <?php
-	$Username = $_POST['firstName'];
-	$Password = $_POST['lastName'];
-	// Database connection
-	$conn = new mysqli('localhost','root','','test');
-	if($conn->connect_error){
-		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("insert into registration(Username, Password) values(?, ?)");
-		$stmt->bind_param("sssssi", $Username, $Password);
-		$execval = $stmt->execute();
-		echo $execval;
-		echo "Registration successfully...";
-		$stmt->close();
-		$conn->close();
-	}
+$Username = $_POST['Username'];
+$Password = $_POST['Password'];
+// Database connection
+
+if ($Username == 'system') {
+	include 'src/login-hub.html';
+}
+
 ?>
